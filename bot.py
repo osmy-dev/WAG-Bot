d65144814a667142ba4c2d81ed09f22811c661e0
Elon Johnny <88237497+johnnywai666@users.noreply.github.com>
2022-11-05 20:05:36 +0800

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+import datetime
+import os
+
+bot = commands.Bot(command_prefix='o?', intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+  print("OceanBot - WAG")
+  print("This Bot Made By OsmyTeam")
+  print("Enabing System...")
+  print("----------")
+  await bot.change_presence(activity=discord.Game("OsmyNetwork"))
+
+@bot.event
+async def on_member_join(member):
+  channel = bot.get_channel() #channel id
+  
+  await channel.send(f"{member.mention} 欢迎来到OsmyNetwork!")
+
+@bot.event
+async def on_member_remove(member):
+  channel = bot.get_channel() #channel id
+  await channel.send(f"{member.mention} 离开了OsmyNetwork")
+
+keep_alive.keep_alive()
+bot.run("") #Bot Token
